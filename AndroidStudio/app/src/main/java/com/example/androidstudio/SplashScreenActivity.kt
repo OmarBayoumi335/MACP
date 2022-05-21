00ac4d2e370dd68ffb429d4ee17796e24d055455
OmarBayoumi335 <bayoumi.1747042@studenti.uniroma1.it>
2022-05-21 10:18:54 +0200

    login

---
+++ AndroidStudio/app/src/main/java/com/example/androidstudio/SplashScreenActivity.kt
@@ -0,0 +1,43 @@
+package com.example.androidstudio
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.animation.AlphaAnimation
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        val splashScreenName = findViewById<TextView>(R.id.splash_screen_name)
+
+        val fadeIn = AlphaAnimation(0.0f, 1.0f)
+        val fadeOut = AlphaAnimation(1.0f, 0.0f)
+        splashScreenName.startAnimation(fadeIn)
+        splashScreenName.startAnimation(fadeOut)
+        fadeIn.duration = 1200
+        fadeIn.fillAfter = true
+        fadeOut.duration = 1200
+        fadeOut.fillAfter = true
+        fadeOut.startOffset = 4200 + fadeIn.startOffset
+
+        val intent = Intent(this, LoginActivity::class.java)
+
+        GlobalScope.launch(Dispatchers.Main) {
+            wait()
+            startActivity(intent)
+            overridePendingTransition(0, 0);
+            finish()
+        }
+    }
+
+    suspend fun wait() {
+//        delay(6600)
+    }
+}
