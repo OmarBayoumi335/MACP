6766e673d4cec638465770157d2f3e998b97ed87
Cristiano Bellucci <bellucci.1760390@studenti.uniroma1.it>
2022-05-23 14:14:38 +0200

    firebase user login added

---
+++ AndroidStudio/app/src/main/java/com/example/androidstudio/classi/User.kt
@@ -0,0 +1,48 @@
+package com.example.androidstudio.classi
+
+class User(username: String, id: String) {
+    private var username: String
+    private var id: String
+    private var friends: List<User>
+
+    constructor() : this("", "#0000000")
+
+    init {
+        this.username = username
+        this.id = id
+        this.friends = emptyList()
+    }
+
+    fun getUsername(): String {
+        return username
+    }
+
+    fun setUsername(name: String) {
+        this.username = name
+    }
+
+    fun getId(): String {
+        return id
+    }
+
+    fun setId(surname: String){
+        this.id = surname
+    }
+
+    fun getFriends(): List<User> {
+        return friends
+    }
+
+    fun setFriends(friends: List<User>){
+        this.friends = friends
+    }
+
+    fun addFriend(friend: User){
+        this.friends += friend
+    }
+
+    override fun toString(): String {
+        return "User(username='$username', id='$id', friends=$friends)"
+    }
+
+}
--- AndroidStudio/app/src/main/java/com/example/androidstudio/login/LoginActivity.kt
+++ AndroidStudio/app/src/main/java/com/example/androidstudio/login/LoginActivity.kt
@@ -4,9 +4,11 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import com.example.androidstudio.home.MenuActivity
 import com.example.androidstudio.R
+import com.example.androidstudio.classi.User
+import com.example.androidstudio.home.MenuActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
@@ -15,8 +17,11 @@ import com.google.android.gms.common.SignInButton
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.*
+import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 
@@ -25,6 +30,7 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener{
     private var clientId = "8984037607-diqinm17j00uucdgkt14jb71seu6qlm1.apps.googleusercontent.com"
     private lateinit var mGoogleSignInClient: GoogleSignInClient
     private lateinit var auth: FirebaseAuth
+    private lateinit var database: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,7 +45,7 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener{
 
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
-        val signInButton = findViewById<SignInButton>(R.id.sign_in_button)
+        val signInButton = findViewById<SignInButton>(R.id.login_sign_in_button)
         signInButton.setSize(SignInButton.SIZE_STANDARD)
 
         signInButton.setOnClickListener(this);
@@ -86,7 +92,7 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener{
                             // Sign in success, update UI with the signed-in user's information
                             Log.i("Login", "signInWithCredential:success")
                             val user = auth.currentUser
-                            signIn()
+                            signIn(user)
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.i("Login", "signInWithCredential:failure", task.exception)
@@ -110,29 +116,117 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener{
 
     override fun onClick(v: View) {
         when (v.id) {
-            R.id.sign_in_button -> {
+            R.id.login_sign_in_button -> {
                 val signInIntent: Intent = mGoogleSignInClient.signInIntent
                 startActivityForResult(signInIntent, 0)
             }
         }
     }
 
-    private fun signIn() {
+    private fun signIn(currentUser: FirebaseUser?) {
+        database = Firebase.database.reference
+        if (currentUser != null) {
+            database.get().addOnSuccessListener {
+                var exist = false
+                var lastUserId = ""
+                for (entry in it.children){
+                    if(entry.key == "UserLastId"){
+                        lastUserId = entry.value as String
+                    } else if(entry.key == "Users"){
+                        for (single_user in entry.children) {
+                            if(single_user.key == currentUser.uid){
+                                exist = true
+                                break
+                            }
+                        }
+                    } else {
+                        continue
+                    }
+                }
+                if(!exist){
+                    Log.i("Login", "User created")
+                    val username = findViewById<EditText>(R.id.login_username_edittext).text.toString()
+                    val id = createId(lastUserId)
+                    val user = User(username, id)
+                    database.child("Users").child(currentUser.uid).setValue(user)
+                    database.child("UserLastId").setValue(id)
+                }
+                val intent = Intent(this, MenuActivity::class.java)
+                startActivity(intent)
+                overridePendingTransition(0, 0);
+                finish()
+            }.addOnFailureListener {
+                Log.e("Login", "Error getting data", it)
+            }
+        }
+    }
 
-        val intent = Intent(this, MenuActivity::class.java)
-        startActivity(intent)
-        overridePendingTransition(0, 0);
-        finish()
+    private fun createId(id: String?): String {
+        var output = ""
+        var carryover = true
+        if (id != null) {
+            for(i in id.length-1 downTo 1 step 1){
+                var c = id[i].toString()
+                if(carryover){
+                    val n = decodeId(c[0]) + 1
+                    c = "0"
+                    if(n < 62){
+                        c = encodeId(n)
+                        carryover = false
+                    }
+                }
+                output = output.plus(c)
+            }
+        }
+        output = output.plus("#")
+        output = output.reversed()
+        return output
+    }
+
+    private fun decodeId(c: Char): Int{
+        val hashcode = c.hashCode()
+        var n = 0
+        if(hashcode in 48..57) { // numbers
+            n = hashcode - 48
+        }
+
+        if(hashcode in 97..122) { // lowercase
+            n = hashcode - 97 + 10
+        }
+
+        if(hashcode in 65..90) { // uppercase
+            n = hashcode - 65 + 10 + 26
+        }
+        return n
+    }
+
+    private fun encodeId(n: Int): String{
+        var index = 0
+        var x = ""
+        if(n < 10) { // numbers
+            index = n + 48
+            x = index.toChar().toString()
+        }
+
+        if(n in 10..10+25) { // lowercase
+            index = n - 10 + 97
+            x = index.toChar().toString()
+        }
+
+        if(n >= 10 +26) { // uppercase
+            index = n - (10 + 26) + 65
+            x = index.toChar().toString()
+        }
+        return x
     }
 
     override fun onStart() {
         super.onStart()
-        //val account = GoogleSignIn.getLastSignedInAccount(this)
-        val currentUser = auth.currentUser
-        Log.i("Login", "" + currentUser?.displayName.toString())
-        if (currentUser != null){
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+        if (account != null){
             Log.i("Login", "Existing login, Logged in")
-            signIn()
+            val currentUser = auth.currentUser
+            signIn(currentUser)
         }
         else {
             Log.i("Login", "Not existing login")
